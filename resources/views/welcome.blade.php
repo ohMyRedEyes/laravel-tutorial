7af20dfaf15e39101548adc70533c80c6778b8e4
igor.v <igor23077@mail.ru>
2020-03-07 23:25:10 +0400

    Add sending data to views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,16 @@
 @extends('layout')
 
 @section('content')
-    <h1>My Home Page</h1>
+    <h1>My {{$foo}} {{$req}} Page</h1>
+
+    <ul>
+    @foreach ($tasks as $task)
+        <li>{{$task}}</li>    
+    @endforeach
+    
+    <!-- traditional PHP way to do it -->
+    <?php foreach ($tasks as $task) : ?>
+        <li><?= $task; ?></li>
+    <?php endforeach; ?>
+    </ul>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = [
+        'Go to the store',
+        'Go to the market',
+        'Go to work',
+    ];
+
+    return view('welcome', [
+        'tasks' => $tasks,
+        'foo' => 'Awesome',
+        'req' => request('title'),  // value taken from address bar '?title=SomeTitle'
+    ]);
+
+    //another ways to return the same
+    return view('welcome')->withTasks($tasks)->withFoo('Awesome');  //'with' will be removed and the rest will
+                                                                    // become a lowecase variable name
+    
+    return view('welcome')->withTasks([
+        'Go to the store',
+        'Go to the market',
+        'Go to work',
+    ]);
+
+    return view('welcome')->with([
+        'tasks' => [
+            'Go to the store',
+            'Go to the market',
+            'Go to work',
+        ],
+        'foo' => 'Awesome',
+        'req' => request('title'),
+    ]);
 });
 
 Route::get('/about', function () {
