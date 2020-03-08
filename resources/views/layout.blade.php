f80b0fcb494659d2cf808a706073f362ca5d4219
igor.v <igor23077@mail.ru>
2020-03-08 14:52:54 +0400

    Add projects' Model, View, and Controller

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,6 +10,7 @@
         <li><a href="/">Home</a></li>
         <li><a href="/about">About Us</a></li>
         <li><a href="/contact">Contact</a></li>
+        <li><a href="/projects">Projects</a></li>
     </ul>
 
     @yield('content')
